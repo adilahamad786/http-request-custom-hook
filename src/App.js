ebbd9b1844686c9efb6decf6bb86a463239ebef8
adilahamad786 <adilahamad.personally@gmail.com>
2022-10-06 17:27:03 +0530

    Make more reuseable useHttp custom hook and use inside App component for Get request.0

--- src/App.js
+++ src/App.js
@@ -7,24 +7,22 @@ import NewTask from './components/NewTask/NewTask';
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = tasksObj => {
-    const loadedTasks = [];
-
-    for (const taskKey in tasksObj) {
-      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  }
-
-  const {isLoading, error, sendRequest : fetchTasks} = useHttp(
-    { url : 'https://all-movie-store-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json'},
-    transformTasks
-  )
+  const {isLoading, error, sendRequest : fetchTasks} = useHttp()
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+
+    const transformTasks = tasksObj => {
+      const loadedTasks = [];
+  
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+  
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks({ url : 'https://all-movie-store-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json'}, transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
