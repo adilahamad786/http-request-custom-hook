4592e47a26fe8c5afb59428c5b7d792597c928ca
adilahamad786 <adilahamad.personally@gmail.com>
2022-10-06 13:23:00 +0530

    Using custom hook useHttp for GET request inside the App component.

--- src/App.js
+++ src/App.js
@@ -5,36 +5,22 @@ import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://all-movie-store-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json'
-      );
+  const transformTasks = tasksObj => {
+    const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
     }
-    setIsLoading(false);
-  };
+
+    setTasks(loadedTasks);
+  }
+
+  const {isLoading, error, sendRequest : fetchTasks} = useHttp(
+    { url : 'https://all-movie-store-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json'},
+    transformTasks
+  )
 
   useEffect(() => {
     fetchTasks();
